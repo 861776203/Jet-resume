7bb192ee13114b54c619c34950752f767a00bc43
马宇狄 <mayud0_0@163.com>
2021-04-22 14:22:22 +0800

    模板布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>aaoo</div>
+    <div class="resume_box">
+        <div class="card">ao</div>
+        <div class="content">
+            内容
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,7 +14,40 @@ export default {
     }
 }
 </script>
-
-<style>
-
+<style lang='scss' scoped>
+.resume_box{
+    display: flex;
+    justify-content: center;
+    min-width: 1150px;
+    padding-top: 20px;
+}
+.card{
+    width: 320px;
+    height: 1000px;
+    border: 1px solid #ccc;
+    margin-right: 30px;
+    border-radius: 5px;
+    box-shadow: 0 5px 20px #bbb;
+}
+.content{
+    width: 750px;
+    height: 2000px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 5px 20px #bbb;
+}
+@media (max-width: 1150px){
+    .resume_box{
+        flex-wrap: wrap;
+        padding: 20px 20px;
+        box-sizing: border-box;
+    }
+    .card{
+        width: 100%;
+        margin: 0;
+    }
+    .content{
+        width: 100%;
+    }
+}
 </style>
